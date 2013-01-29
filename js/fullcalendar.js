cbd165a948db12ade9a531a1a8e4190840c67585
Johan Adell <johan@upsideduck.com>
2013-01-29 08:46:24 +0100

    FIX: Fill calendar width

--- js/fullcalendar.js
+++ js/fullcalendar.js
@@ -3781,7 +3781,7 @@ function AgendaEventRenderer() {
 			forward = 0;
 			leftmost = colContentLeft(colI*dis + dit);
 			availWidth = colContentRight(colI*dis + dit) - leftmost;
-			availWidth = Math.min(availWidth-6, availWidth*.95); // TODO: move this to CSS
+			//availWidth = Math.min(availWidth-6, availWidth*.95); // TODO: move this to CSS
 			if (levelI) {
 				// indented and thin
 				outerWidth = availWidth / (levelI + forward + 1);
